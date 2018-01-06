bee0d32acd4999efb733d04b1ba1ebf18db9f4cd
Yato7908 <35040538+Yato7908@users.noreply.github.com>
2018-01-07 00:27:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ client.on("message", async message => {
     // This command must be limited to mods and admins. In this example we just hardcode the role names.
     // Please read on Array.some() to understand this bit:
     // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-    if(!message.member.roles.some(r=>["Owner-san", "Meddling Mods"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["Owner-san", "Meddling Mods", "Mods", "Admin", "Moderators"].includes(r.name)) )
       return message.reply("Bitch! You ain't got enough powah to do that shit!");
 
     // Let's first check if we have a member and if we can kick them!
@@ -99,7 +99,7 @@ client.on("message", async message => {
   if(command === "ban") {
     // Most of this command is identical to kick, except that here we'll only let admins do it.
     // In the real world mods could ban too, but this is just an example, right? ;)
-    if(!message.member.roles.some(r=>["Owner-san"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["Owner-san", "Owner", "Admin"].includes(r.name)) )
       return message.reply("You can't slam that ban hammer on ass if you don't have the power!");
 
     let member = message.mentions.members.first();
